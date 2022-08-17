822390406a73b65e0b3b76722288480481b049c8
student <student@BC-minimac023s-Mac-mini.local>
2022-08-17 12:50:32 +0300

    added salary calculator

---
+++ 220816_week2/task_salary_calculator/main.js
@@ -0,0 +1,21 @@
+let salary, hours;
+salary = prompt("Enter your hourly salary");
+hours = prompt("Enter how many hours you have worked");
+let output;
+function calc(salary, hours) {
+  if (salary < 0 || hours < 0) {
+    output = "Invalid data";
+  } else {
+    output = salary * hours;
+    if (hours > 7) {
+      output += salary * 0.5 * (hours - 7);
+      if (hours > 9) output += salary * 0.5 * (hours - 9);
+    }
+  }
+}
+calc(salary, hours);
+console.log(output);
+document.getElementById(
+  "user_input"
+).innerHTML = `Hourly salary: ${salary}, hours worked: ${hours}`;
+document.getElementById("user_result").innerHTML = `Your earned: ${output}`;
