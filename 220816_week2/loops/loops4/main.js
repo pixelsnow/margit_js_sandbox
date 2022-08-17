4440ab845b81e778719cf34895bfde8d8f48de1c
student <student@BC-minimac023s-Mac-mini.local>
2022-08-17 14:55:47 +0300

    added loops 5 and modified some others a bit

--- 220816_week2/loops/loops4/main.js
+++ 220816_week2/loops/loops4/main.js
@@ -1,7 +1,7 @@
 /* Make a program that asks 20 numbers from user. After that the program prints out how many of those numbers where even.*/
 
 let counter = 0;
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 4; i++) {
   if (+prompt(`Enter the ${i}th number`) % 2 == 1) counter++;
 }
 alert(`${counter} of your numbers were even`);
---
+++ 220816_week2/loops/loops5/main.js
@@ -0,0 +1,12 @@
+/* Make a program that asks numbers from the user, until user gives 0 and then program ends.
+In the end program prints out average of the numbers.*/
+
+let num;
+let sum = 0;
+let count = 0;
+do {
+  num = +prompt("Enter a number");
+  sum += num;
+  count++;
+} while (num);
+alert(`average of your numbers is ${sum / count}`);
