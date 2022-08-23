18362b51cd79b6aee91fa2d4a9085a645619ed6f
student <student@BC-minimac023s-Mac-mini.local>
2022-08-23 10:58:23 +0300

    replaced off with even in loops4

--- 220816_week2/loops/loops4/main.js
+++ 220816_week2/loops/loops4/main.js
@@ -3,14 +3,11 @@
 let counter = 0;
 let ending;
 for (let i = 1; i <= 20; i++) {
-  if (i === 1) // Making the input pretty
-    ending = 'st';
-  else if (i === 2)
-    ending = 'nd';
-  else if (i === 3)
-    ending = 'rd';
-  else ending = 'th';
+  if (i === 1) ending = "st"; // Making the input pretty
+  else if (i === 2) ending = "nd";
+  else if (i === 3) ending = "rd";
+  else ending = "th";
 
-  if (+prompt(`Enter the ${i}${ending} number`) % 2 == 1) counter++;
+  if (+prompt(`Enter the ${i}${ending} number`) % 2 == 0) counter++;
 }
 alert(`${counter} of your numbers were even`);
