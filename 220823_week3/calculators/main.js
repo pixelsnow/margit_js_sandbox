9ae9e4b5c0321064834d08248c825886b3d6fb51
student <student@BC-minimac023s-Mac-mini.local>
2022-08-24 14:14:26 +0300

    removed comments

--- 220823_week3/calculators/main.js
+++ 220823_week3/calculators/main.js
@@ -1,4 +1,4 @@
-function displayPetrol() {
+displayPetrol = () => {
   const price = +document.querySelector("#price").value;
   const money = +document.querySelector("#money").value;
   const answer = document.querySelector("#user_output");
@@ -9,16 +9,8 @@ function displayPetrol() {
     : (answer.textContent = `You have ${(money / price).toFixed(
         1
       )} liters of petrol. Oops, you have to stay here...`);
-  /* if (money / price > 10)
-    answer.textContent = `You have ${(money / price).toFixed(
-      1
-    )} liters of petrol. Good, you can escape now!`;
-  else
-    answer.textContent = `You have ${(money / price).toFixed(
-      1
-    )} liters of petrol. Oops, you have to stay here...`; */
   console.log(`price is ${price}, money is ${money}`);
-}
+};
 
 tempConverter = (inputId, input) => {
   const idF = document.querySelector("#fahrenheit");
