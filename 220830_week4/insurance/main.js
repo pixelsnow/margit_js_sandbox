51057e0d7cc5e7f1d4fdfcfabf06b8f70bb3b55b
Vagapova Valeriia <s2200750@Lera-MacBook-Air.local>
2022-08-31 14:07:25 +0300

    added insurance and modified pizza a bit

---
+++ 220830_week4/insurance/main.js
@@ -0,0 +1,53 @@
+const form = document.querySelector("form");
+const username = document.querySelector("#name");
+const age = document.querySelector("#age");
+const health = document.querySelectorAll(".health");
+const habits = document.querySelectorAll(".habits");
+const result = document.querySelector("#result");
+
+const calcScore = (e) => {
+  e.preventDefault();
+
+  console.log(username.value);
+
+  let res = 500;
+  let increase = 0;
+  if (age.value >= 18) increase += 10;
+  if (age.value >= 26) increase += 20;
+  if (age.value >= 36) increase += 30;
+  if (age.value >= 46) increase += 40;
+  if (age.value >= 56) increase += 50;
+  if (age.value >= 66) increase += 60;
+  res += res * increase * 0.01;
+
+  console.log("age increase: ", increase);
+  console.log("current result: " + res);
+
+  increase = 0;
+  health.forEach((item) => {
+    if (item.checked) increase++;
+  });
+  res += res * increase * 0.01;
+
+  console.log("health increase: ", increase);
+  console.log("current result: " + res);
+
+  increase = 0;
+  habits.forEach((item) => {
+    if (item.checked) {
+      if (item.value === "good") increase -= 5;
+      else if (item.value === "bad") increase += 5;
+    }
+  });
+  res += res * increase * 0.01;
+
+  console.log("habit increase: ", increase);
+  console.log("current result: " + res);
+
+  document.querySelector("#result").textContent = `${
+    username.value
+  }, your result is ${res.toFixed(0)}`;
+  form.reset();
+};
+
+form.addEventListener("submit", calcScore);
