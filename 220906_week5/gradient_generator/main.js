8bdcb9e573fc8e69411c176c18a1774551057985
Vagapova Valeriia <s2200750@Lera-MacBook-Air.local>
2022-09-06 22:20:28 +0300

    added gradient generator, direction labels still need work

---
+++ 220906_week5/gradient_generator/main.js
@@ -0,0 +1,35 @@
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.querySelector("body");
+const dirs = document.querySelectorAll('input[name="dir"]');
+const css = document.querySelector("#css");
+let dir = "bottom";
+let c1 = "#98ecc0";
+let c2 = "#9e7cee";
+
+const updateBackground = () => {
+  body.style.background = `linear-gradient(to ${dir}, ${c1}, ${c2}) no-repeat fixed
+    center center/cover`;
+  css.textContent = `linear-gradient(to ${dir}, ${c1}, ${c2});`;
+};
+
+color1.addEventListener("input", () => {
+  c1 = color1.value;
+  updateBackground();
+});
+
+color2.addEventListener("input", () => {
+  c2 = color2.value;
+  updateBackground();
+});
+
+dirs.forEach((direction) =>
+  direction.addEventListener("change", () => {
+    dir = direction.value;
+    updateBackground();
+  })
+);
+
+color1.value = c1;
+color2.value = c2;
+updateBackground();
