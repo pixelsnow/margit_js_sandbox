7e6684bb89f50f32a8072eb9a9187ccce1e75f14
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-13 12:22:06 +0300

    added Margit's version of BG generator

---
+++ 220906_week5/bg_generator_margit/main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("#bg_generator");
+
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let direction = document.querySelectorAll('input[name="direction"]');
+
+let text = document.querySelector("p");
+
+const setGradient = (event) => {
+  event.preventDefault();
+  let selectedValue;
+  for (const sel of direction) {
+    if (sel.checked) {
+      selectedValue = sel.value;
+    }
+  }
+
+  let gradient = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`;
+
+  document.body.style.backgroundImage = gradient;
+  text.textContent = gradient + ";";
+};
+
+form.addEventListener("change", setGradient);
