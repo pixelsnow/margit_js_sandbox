1d2aa47fc86a0f039dbb6c535eb37af45abba30e
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-28 20:25:44 +0300

    broken image order, fetch needs fixing

--- 220927_week8/pokedex/main.js
+++ 220927_week8/pokedex/main.js
@@ -3,35 +3,56 @@ const search = document.querySelector("#search");
 const cards = document.querySelector(".card-container");
 
 let pokeData = [];
+let imgLink;
 
 const fetchPoke = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
     .then((response) => response.json())
     .then((data) => {
       pokeData = data.results;
-      console.log(pokeData);
       pokeCards();
     });
 };
 
+const fetchImg = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      cards.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card">
+          <div class="image"><img src="${data.sprites.other.dream_world.front_default}" alt="${data.species.name} picture"></div>
+          <div class="info"><h2>${data.species.name}</h2></div>
+        </div>`
+      );
+    });
+};
+
 const pokeCards = () => {
-  pokeData.forEach((pokemon, i) => {
-    console.log(i);
-    cards.insertAdjacentHTML(
+  pokeData.forEach((pokemon) => {
+    console.log();
+    imgLink = fetchImg(pokemon.url);
+    /* cards.insertAdjacentHTML(
       "beforeend",
       `<div class="card">
-        <div class="image"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-          i + 1
-        }.svg" alt="${pokemon.name} picture"></div>
+        <div class="image"><img src="${imgLink}" alt="${pokemon.name} picture"></div>
         <div class="info"><h2>${pokemon.name}</h2></div>
       </div>`
-    );
+    ); */
   });
 };
 
 fetchPoke();
 
 /* 
+
+
+https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+          i + 1
+        }.svg
+
+
+        
 const fetchPoke = async () => {
   const response = await fetch(
     "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
