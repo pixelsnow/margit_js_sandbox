e8b5ad3a3eeec47f25f118674b78c61b52933b35
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-09-28 23:24:24 +0300

    fetched all info into an array. got type images working

--- 220927_week8/pokedex/main.js
+++ 220927_week8/pokedex/main.js
@@ -3,102 +3,125 @@ const search = document.querySelector("#search");
 const cards = document.querySelector(".card-container");
 
 let pokeData = [];
-let imgLink;
-let pokeData2 = [];
 
+const getTypeImg = (type) => {
+  switch (type) {
+    case "bug":
+      return "https://upload.wikimedia.org/wikipedia/commons/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg";
+    case "dark":
+      return "https://upload.wikimedia.org/wikipedia/commons/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg";
+    case "ghost":
+      return "https://upload.wikimedia.org/wikipedia/commons/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg";
+    case "grass":
+      return "https://upload.wikimedia.org/wikipedia/commons/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg";
+    case "dragon":
+      return "https://upload.wikimedia.org/wikipedia/commons/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg";
+    case "electric":
+      return "https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg";
+    case "fairy":
+      return "https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg";
+    case "fighting":
+      return "https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg";
+    case "fire":
+      return "https://upload.wikimedia.org/wikipedia/commons/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg";
+    case "flying":
+      return "https://upload.wikimedia.org/wikipedia/commons/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg";
+    case "ground":
+      return "https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg";
+    case "ice":
+      return "https://upload.wikimedia.org/wikipedia/commons/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg";
+    case "normal":
+      return "https://upload.wikimedia.org/wikipedia/commons/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg";
+    case "poison":
+      return "https://upload.wikimedia.org/wikipedia/commons/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg";
+    case "psychic":
+      return "https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg";
+    case "rock":
+      return "https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg";
+    case "steel":
+      return "https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg";
+    case "water":
+      return "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg";
+    default:
+      return "#";
+  }
+};
+
+// Renders to HTML
 const renderCards = () => {
-  pokeData2.forEach((card) => {
+  pokeData.forEach((card) => {
+    let typesHtml = "";
+    card.types.forEach(
+      (type) =>
+        (typesHtml += `<img src=${getTypeImg(type)} alt="${type}"></img>`)
+    );
     cards.insertAdjacentHTML(
       "beforeend",
       `<div class="card">
         <div class="image"><img src="${card.img}" alt="${card.name} picture"></div>
-        <div class="info"><h2>${card.name}</h2></div>
+        <div class="info"><h2>#${card.id} ${card.name}</h2><div class="types">${typesHtml}</div></div>
       </div>`
     );
   });
 };
 
-const fetchPoke = async () => {
-  const response = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
-  );
-  const data = await response.json();
-  pokeData = data.results;
-  await pokeCards();
-  renderCards();
+// Returns a link to an image
+const setImg = (data) => {
+  let img = data.sprites.other.dream_world.front_default;
+  if (!img) img = data.sprites.other["official-artwork"].front_default;
+  if (!img) img = data.sprites.other.home.front_default;
+  if (!img) img = data.sprites.front_default;
+  if (!img)
+    img =
+      "https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg";
+  return img;
 };
 
-const pokeCards = async () => {
-  pokeData2.length = pokeData.length;
+// Returns an array of types
+const getTypes = (types) => {
+  let res = [];
+  types.forEach((type) => {
+    res.push(type.type.name);
+  });
+  return res;
+};
+
+// Rewrites pokeData with full info
+const fillPokeData = async () => {
   for (let i = 0; i < pokeData.length; i++) {
+    // Fetching pokemon data from the url
     const response = await fetch(pokeData[i].url);
     const data = await response.json();
-    let img = data.sprites.other["official-artwork"].front_default;
-    if (!img) img = data.sprites.other.dream_world.front_default;
-    if (!img) img = data.sprites.other.home.front_default;
-    pokeData2[i] = { name: pokeData[i].name, img: img };
+    const img = setImg(data);
+    const types = getTypes(data.types);
+    // Fetching generation info from the species page
+    const response2 = await fetch(data.species.url);
+    const data2 = await response2.json();
+    const gen = +data2.generation.url[data2.generation.url.length - 2];
+    // Saving info into an array of objects
+    pokeData[i] = {
+      id: data.id,
+      name: data.name,
+      img: img,
+      gen: gen,
+      types: types,
+    };
   }
 };
 
-const fetchImg = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      /* cards.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card">
-          <div class="image"><img src="${data.sprites.other.dream_world.front_default}" alt="${data.species.name} picture"></div>
-          <div class="info"><h2>${data.species.name}</h2></div>
-        </div>`
-      ); */
-    });
-};
-
-const pokeCards2 = () => {
-  pokeData.forEach((pokemon, i) => {
-    fetch(pokemon.url)
-      .then((response) => response.json())
-      .then((data) => {
-        let img = data.sprites.other.dream_world.front_default;
-        if (img) img = data.sprites.other["official-artwork"].front_default;
-
-        /* cards.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card">
-          <div class="image"><img src="${data.sprites.other.dream_world.front_default}" alt="${data.species.name} picture"></div>
-          <div class="info"><h2>${data.species.name}</h2></div>
-        </div>`
-      ); */
-      });
-  });
+const fetchPoke = async () => {
+  // Fetching the full list of pokemons in the database
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0"
+  );
+  const data = await response.json();
+  // Saving the list to the array
+  pokeData = data.results;
+  // Getting full info about all pokemons and rewriting the array
+  await fillPokeData();
+  console.table(pokeData);
+  // Rendering to HTML
+  renderCards();
 };
 
 fetchPoke();
-
-/* 
-
-
-https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-          i + 1
-        }.svg
-
-
-        
-const fetchPoke = async () => {
-  const response = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
-  );
-  const data = await response.json();
-  data.results.forEach((pokemon) => pokemons.push(pokemon.name));
-  console.log(pokemons);
-  pokemons.forEach((pokemon) => {
-    cards.insertAdjacentHTML(
-      "beforeend",
-      `<div class="card">
-    <div class="image"></div>
-    <div class="info"><h2>${pokemon}</h2></div>
-  </div>`
-    );
-  });
-};
- */
